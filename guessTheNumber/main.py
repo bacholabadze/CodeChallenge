203878e68f242ca11f1ed0ecadf97c7b62a553b6
ambri <bachukilabadze@gmail.com>
2021-12-15 16:03:47 -0500

    add number guessing console game

---
+++ guessTheNumber/main.py
@@ -0,0 +1,39 @@
+import random
+
+
+def greeting():
+    print("""
+    Welcome to the Number Guessing Game!
+    I'm thinking of a number between 1 and 100.
+    Type 'easy' or 'hard'.
+    """)
+    difficulty = input('Choose a difficulty: ')
+    if difficulty.lower() == 'hard':
+        # number of lives
+        return 5
+    else:
+        # number of lives
+        return 10
+
+
+def playGame():
+    lives = greeting()
+    number = random.randint(0, 100)
+    print(f'Lives remaining: {lives}')
+    while lives:
+        print(f"You have {lives} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        if guess > number:
+            lives -= 1
+            print("Too high.")
+        elif guess < number:
+            lives -= 1
+            print('Too low.')
+        else:
+            print("You guessed the number!")
+            break
+    else:
+        print("You lost")
+
+
+playGame()
