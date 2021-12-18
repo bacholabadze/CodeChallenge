9c65abd7a9d90644f5049b93d6f7ebe9018d8cec
ambri <bachukilabadze@gmail.com>
2021-12-18 16:02:32 -0500

    add snake game project

---
+++ snake_game/main.py
@@ -0,0 +1,51 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+WIDTH = 600
+HEIGHT = 800
+
+screen = Screen()
+screen.setup(width=WIDTH, height=HEIGHT)
+screen.bgcolor("black")
+screen.title("The Amazing Snake")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.score_up()
+
+    # Detect collision with wall
+    if snake.head.xcor() > WIDTH/2 - 20 or snake.head.xcor() < -WIDTH/2 + 20\
+            or snake.head.ycor() > HEIGHT/2 - 20 or snake.head.ycor() < -HEIGHT/2+20:
+
+        scoreboard.game_over()
+        game_is_on = False
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 15:
+            scoreboard.game_over()
+            game_is_on = False
+
+screen.exitonclick()
---
+++ snake_game/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 330)
+        self.score = 0
+        self.color("white")
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def score_up(self):
+        self.clear()
+        self.score += 5
+        self.update_scoreboard()
